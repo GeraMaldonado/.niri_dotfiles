20e0e6279f146574844423b126a9f18ec318f1ba
GeraMaldonado <gmaldonadofelix@gmail.com>
2026-09-18 22:18:26 -0600

    limpiando sistema

--- niri/autostart.sh
+++ niri/autostart.sh
@@ -2,24 +2,11 @@
 
 # Set cursor theme for niri / Wayland (and Xwayland apps)
 # Must match an installed theme folder under ~/.icons or /usr/share/icons
-export XCURSOR_THEME="Bibata-Modern-Classic"
-export XCURSOR_SIZE=24
-export XCURSOR_PATH="$HOME/.config/niri:$HOME/.icons:$HOME/.local/share/icons:/usr/share/icons"
-
-# Wallpaper
-#swaybg -i "$HOME/Imágenes/wallpapers/764519.jpg" -m fill &
-
-# Notifications
-#mako &
-
-# Bar
-#waybar &
+#export XCURSOR_THEME="Bibata-Modern-Classic"
+#export XCURSOR_SIZE=24
+#export XCURSOR_PATH="$HOME/.config/niri:$HOME/.icons:$HOME/.local/share/icons:/usr/share/icons"
 
 qs -c noctalia-shell &
 
-# Polkit (needed for mounts, permissions)
- /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
+/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
 
-#Gestor de Clipboard
-copyq &
-#wl-paste --watch wl-copy &
